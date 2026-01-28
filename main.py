14be9555f795ce2bf1531e3becff8c4c1660d2b7
Rizwan <rizwanrahim546@gmail.com>
2026-01-28 21:43:51 +0500

    Fix port binding using env PORT for deployment

--- main.py
+++ main.py
@@ -108,5 +108,5 @@ def ask(q: Query):
 # ------------------ RUN SERVER ------------------
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
